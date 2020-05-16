f35362e8583d0dfa715bc228ce00f7468aa91d5e
Phua Yue Jun <phuayj@seagroup.com>
2020-05-16 15:56:18 +0800

    Add image file name to the grid

--- app/src/main/java/com/example/simpleimageviewer/ImageGridAdapter.kt
+++ app/src/main/java/com/example/simpleimageviewer/ImageGridAdapter.kt
@@ -2,12 +2,17 @@ package com.example.simpleimageviewer
 
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.davemorrissey.labs.subscaleview.ImageSource
 
-class ImageViewHolder(val imageView: SubsamplingScaleImageView) : RecyclerView.ViewHolder(imageView)
+class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val image: SubsamplingScaleImageView = itemView.findViewById(R.id.image_display)
+    val text: TextView = itemView.findViewById(R.id.image_text)
+}
 
 class ImageGridAdapter(data: Array<String> = arrayOf()) :
     RecyclerView.Adapter<ImageViewHolder>() {
@@ -21,20 +26,19 @@ class ImageGridAdapter(data: Array<String> = arrayOf()) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        // create new ImageView
         val view = layoutInflater.inflate(R.layout.box_image, parent, false)
-                as SubsamplingScaleImageView
         return ImageViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         val imagePath = imageList[position]
-        holder.imageView.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_CROP)
-        holder.imageView.setImage(ImageSource.uri(imagePath))
-        holder.imageView.setOnClickListener {
+        holder.image.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_CROP)
+        holder.image.setImage(ImageSource.uri(imagePath))
+        holder.image.setOnClickListener {
             it.context.startActivity(Intent(it.context, ImageActivity::class.java).apply {
                 putExtra("imagePath", imagePath)
             })
         }
+        holder.text.text = imagePath.substring(imagePath.lastIndexOf("/") + 1)
     }
 }
